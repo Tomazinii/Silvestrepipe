dd1ba6c0bcc7bf69809341f8fd3d23d9b1a3ea06
j <j>
2022-02-23 09:42:30 -0300

    strName

--- api/models.py
+++ api/models.py
@@ -5,3 +5,8 @@ class Testes(models.Model):
 	name = models.CharField(max_length=50)
 	last_name = models.CharField(max_length=50,default="")
 	kaqui = models.CharField(max_length=50,default="")
+
+
+	def __str__(self):
+		return self.name
+	
