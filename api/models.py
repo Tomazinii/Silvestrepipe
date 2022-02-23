80a294bc1855c47434a5497c20afd6fe8b1c9647
j <j>
2022-02-23 09:05:06 -0300

    update1.1

--- api/models.py
+++ api/models.py
@@ -3,4 +3,5 @@ from django.db import models
 # Create your models here
 class Testes(models.Model):
 	name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50,default="")
 	kaqui = models.CharField(max_length=50,default="")
